0bf0b4e2160c8f01cb2624b2e292fe8c4403383f
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-19 18:51:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "male", "brown", "quite", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "male", "brown", "quite", 1)
 
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "brown", "ferocious", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "brown", "ferocious", 1)
 
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "female", "black", "friendly", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "female", "black", "friendly", 1)
 
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "ferocious", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "ferocious", 0)
 
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "female", "brown", "brave", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "female", "brown", "brave", 1)
 
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "male", "white", "brave", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "male", "white", "brave", 1)
 
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "female", "white", "quite", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "female", "white", "quite", 1)
 
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "male", "black", "ferocious", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "male", "black", "ferocious", 1)
 
