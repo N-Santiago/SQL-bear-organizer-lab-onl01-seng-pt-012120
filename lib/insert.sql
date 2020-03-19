6bba33f36a361f86d1ecd67c8dffec49140c8813
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-19 18:50:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,13 +4,13 @@ INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Rowdy",
 
 INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "female", "black", "friendly", 1)
 
-NSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "ferocious", 0)
+INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "ferocious", 0)
 
-NSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "female", "brown", "brave", 1)
+INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "female", "brown", "brave", 1)
 
-NSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "male", "white", "brave", 1)
+INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "male", "white", "brave", 1)
 
-NSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "female", "white", "quite", 1)
+INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "female", "white", "quite", 1)
 
 NSERT INTO bear (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "male", "black", "ferocious", 1)
 
