835f8178c39febffe1f7ba3c7790db7c4e779363
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-19 18:41:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "male", "brown", "quite", 1)
+
+INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "brown", "", 1)
